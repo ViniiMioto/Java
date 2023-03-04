11bf5c8dc97c0af8bb3bce3da57d274fcef5fd53
ViniiMioto <viniciusmioto2004@outlook.com.br>
2023-03-03 22:09:08 -0300

    aula 1 LPOO

---
+++ README.md
@@ -0,0 +1,5 @@
+Aqui estarei adicionando todas as atividades e projetos realizados na LPOO Java <br/>
+
+Cada uma das pastas contém uma atividade realizada no dia mencionada pela mesma, juntamente á uma anotação sobre a aula <br/>
+
+Att. Mioto
---
+++ aula27022023_Calculadora/src/aula27022023_calculadora/Aula27022023_Calculadora.java
@@ -0,0 +1,12 @@
+package aula27022023_calculadora;
+
+public class Aula27022023_Calculadora
+{
+
+    public static void main(String[] args)
+    {
+        FormPrincipal frmP = new FormPrincipal(null, true);
+        frmP.setVisible(true);
+    }
+
+}
